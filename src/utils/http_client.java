1a7febd53ab39c8bb2ae59bf10d912ce82461610
jeff chang <397068246@qq.com>
2016-02-17 17:24:43 +0800

    change project name

--- src/utils/http_client.java
+++ src/utils/http_client.java
@@ -8,29 +8,13 @@ import java.io.UnsupportedEncodingException;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 public class http_client {
-	/*
-	public static void main(String[] args) throws ClientProtocolException,
-			IOException {
-		CloseableHttpClient client = HttpClients.createDefault();
-		HttpGet get = new HttpGet(
-				"http://10.134.15.20:8080/sugg/ajaj_json.jsp?key=liudehua&suguuid=0&ip=36.110.147.198&iploc=10.134.107.103&ad_type=1&type=websugg&webfront=websugg&abtestid=7&suid=3&yyid=4&pid=sogou&policyno=7&mfp=8&hs=9&mp=0");
-		HttpResponse response = client.execute(get);
-		System.out.println(response.getProtocolVersion());
-		System.out.println(response.getStatusLine().getStatusCode());
-		System.out.println(response.getStatusLine().getReasonPhrase());
-		System.out.println(response.getStatusLine().toString());
-		HttpEntity entity = response.getEntity();
-		System.out.println(convertStreamToString(entity.getContent()));
-	}
-	*/
 	
-	public static String send_get_request(String url) throws UnsupportedEncodingException, UnsupportedOperationException, IOException {
+	public static String sendGetRequest(String url) throws UnsupportedEncodingException, UnsupportedOperationException, IOException {
 		HttpGet get = new HttpGet(url);
 		resp = client.execute(get);
 		entity = resp.getEntity();
